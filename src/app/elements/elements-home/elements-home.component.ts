79742abc38438a5cc4cdacb7d85b5078b22a3f61
np <ksoopers@outlook.com>
2024-02-13 21:02:26 +0700

    mba 3.12.24

--- src/app/elements/elements-home/elements-home.component.ts
+++ src/app/elements/elements-home/elements-home.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-elements-home',
   templateUrl: './elements-home.component.html',
-  styleUrl: './elements-home.component.css'
+  styleUrl: './elements-home.component.css',
 })
-export class ElementsHomeComponent {
-
-}
+export class ElementsHomeComponent {}
--- src/app/elements/placeholder/placeholder.component.ts
+++ src/app/elements/placeholder/placeholder.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Lines {
+  lineNum: number;
+  widths: number[];
+}
 
 @Component({
   selector: 'app-placeholder',
   templateUrl: './placeholder.component.html',
-  styleUrl: './placeholder.component.css'
+  styleUrl: './placeholder.component.css',
 })
 export class PlaceholderComponent {
+  p_lines: Lines[] = [];
+  img_lines: Lines[] = [];
 
+  @Input() set lines(n: number) {
+    PlaceholderComponent.setLine(this.img_lines, 4, 6);
+    PlaceholderComponent.setLine(this.p_lines, n - 4, 12);
+  }
+
+  private static setLine(lines: Lines[], n: number, limit: number): void {
+    for (let i = 0; i < n; i++) {
+      let line: Lines = { lineNum: i, widths: [] };
+      let width = 0;
+      while (true) {
+        let w = Math.floor(Math.random() * limit);
+        if (w === 0) continue;
+        width += w;
+        if (width > limit - 1) {
+          break;
+        } else {
+          line.widths.push(w);
+        }
+      }
+      lines.push(line);
+    }
+  }
 }
